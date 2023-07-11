c2ab00276053411bd5b074c0cbcc7d3c06ba645b
HectorLPE <hectorlpescobar@gmail.com>
2023-07-11 08:53:31 -0400

    Adding error boundaries and adjusting styling

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-        <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800'}}>
+        <div style={{ overflowY: 'scroll', border: '5px solid green', height: '70vh' }}>
             {props.children}
         </div>
     );
-};
+}
 
 export default Scroll;
